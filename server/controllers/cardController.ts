dd3427cc367c789c5c2782f770d16438c199ff1b
Dawid <dknecksnap@gmail.com>
2025-12-02 00:48:22 +0100

    delete all

--- server/controllers/cardController.ts
+++ server/controllers/cardController.ts
@@ -26,9 +26,56 @@ export async function getAll(
     const pipeline = redis.multi();
     keys.forEach((key) => pipeline.get(key));
     const results = await pipeline.exec();
+    const length = await redis.sCard("cards:all");
 
-    res.json({ status: "success", data: results });
+    res.json({ status: "success", data: { keys, results, length } });
   } catch (err) {
     res.status(500).json({ error: "Failed to fetch from Redis." });
+  }
+}
+
+export async function remove(
+  req: Request,
+  res: Response,
+  redis: RedisClientType
+): Promise<void> {
+  try {
+    const key = req.query.key as string;
+    if (!key) {
+      res.status(400).json({ error: "Key required" });
+      return;
+    }
+    await redis.del(key);
+    await redis.sRem("cards:all", key);
+    console.log("Card removed from DB.");
+    res.json({ status: "success" });
+  } catch (err) {
+    res.status(500).json({ error: "Failed to delete in Redis." });
+  }
+}
+
+export async function removeAll(
+  req: Request,
+  res: Response,
+  redis: RedisClientType
+): Promise<void> {
+  try {
+    const luaScript = `
+      local ids = redis.call('SMEMBERS', 'cards:all')
+      for i=1,#ids do
+        redis.call('DEL', 'card:' .. ids[i])
+      end
+      redis.call('DEL', 'cards:all')
+      return #ids
+    `;
+    const deletedCount = await redis.eval(luaScript, {
+      keys: [],
+      arguments: [],
+    });
+    res
+      .status(200)
+      .json({ message: `Deleted ${deletedCount} cards successfully.` });
+  } catch (err) {
+    res.status(500).json({ error: "Failed to delete all in Redis." });
   }
 }
--- server/routes/api.ts
+++ server/routes/api.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { update, getAll } from "../controllers/cardController.ts";
+import {
+  update,
+  getAll,
+  remove,
+  removeAll,
+} from "../controllers/cardController.ts";
 
 const router = Router();
 
 router.post("/update", (req, res) => update(req, res, req.app.locals.redis));
 router.get("/get", (req, res) => getAll(req, res, req.app.locals.redis));
+router.delete("/delete", (req, res) => remove(req, res, req.app.locals.redis));
+router.delete("/delete-all", (req, res) =>
+  removeAll(req, res, req.app.locals.redis)
+);
 
 export default router;
